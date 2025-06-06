899c1bcbc378ef87757c3ec3a400d0767ca4245e
Subroto89 <me.subrotosarker@gmail.com>
2025-06-06 13:10:03 +0600

    Why Choose Us Component Added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Banner from '../../Components/Home_Component/Banner';
+import Facility from '../../Components/Home_Component/Facility';
 
 const Home = () => {
+    const facilitiesPromise = fetch('facilities.json').then(res => res.json());
     return (
         <div>
            {/* Banner Component */}
            <Banner></Banner>
+
+           {/* Why Choose Use Component */}
+           <Suspense fallback={<div className="text-center text-gray-500">Loading facilities...</div>}>
+                <Facility facilities={facilitiesPromise} />
+           </Suspense>
         </div>
     );
 };
